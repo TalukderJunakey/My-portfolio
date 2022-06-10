9bd883350c7f61abd374c76f251d5024dfaad2d7
Junakey <junakuytalukder@gmail.com>
2022-06-10 22:10:04 +0600

    last commit

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import cover from '../../images/cover.jpg';
+import resume from '../../../src/JUNAKEY_RESUME.pdf'
 const Banner = () => {
     return (
         <div class="hero min-h-screen bg-base-200">
@@ -9,8 +10,8 @@ const Banner = () => {
                     <h1 class="text-5xl font-bold ">--I'M JUNAKEY</h1>
                     <p class="py-6">I'm a frontend developer focused on crafting <br />
                         & user-friendly experiences.I'm passionate building excellent websites.</p>
-                    <button class="btn btn-primary">See Resume
-                        <a class="link" target="_blank" href=""></a>
+                    <button class="btn btn-primary">
+                        <a href={resume} download="JUNAKEY_RESUME.pdf">Download Resume</a>
                     </button>
                 </div>
             </div>
